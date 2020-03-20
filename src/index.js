c4a2191826dd67ad607322e5118f123b75a9c331
batonsosmetanoy <madraimovmsh@gmail.com>
2020-03-20 13:30:44 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,50 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+         '':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let strWithCommas = '';
+
+    for (let i = 0; i < expr.length; i++) {
+      if (i % 10 === 0 && i !== 0) strWithCommas += ',';
+      strWithCommas += expr[i];
+    }
+    
+    let expr2Arr = strWithCommas.split(',');
+    let morseArr = expr2Arr.map(item => numsStr2Morse(item));
+
+    return morseArr.map(item => morse2text(item)).join('');
+}
+
+function numsStr2Morse(str) {
+  let commas = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (i % 2 === 0 && i !== 0) commas += ',';
+    commas += str[i];
+  }
+
+  let commas2arr = commas.split(',');
+  let morse ='';
+
+  for (let i = 0; i < commas2arr.length; i++) {
+    if (commas2arr[i] === '10') morse += '.';
+    if (commas2arr[i] === '11') morse += '-';
+  }
+
+  return morse;
+}
+
+function morse2text(str) {
+  let text = '';
+
+  for (let key in MORSE_TABLE) {
+    if (str === key) text += MORSE_TABLE[key];
+  }
+  
+  return text;
 }
 
 module.exports = {
